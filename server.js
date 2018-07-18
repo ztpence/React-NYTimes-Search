26970df75f92214afec0a53cd7a0f890a90c0fb5
Zane P <ztpence@gmail.com>
2018-07-17 17:49:14 -0700

    trying to pass option into mongoose connect() call

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/nytreact",
+  { useNewUrlParser: true }
+);
 
 // Start the API server
 app.listen(PORT, function() {
