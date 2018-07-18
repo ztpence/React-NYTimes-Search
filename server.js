017107ebdc641b3b516098d51fc70a822b706d49
Zane P <ztpence@gmail.com>
2018-07-17 17:40:30 -0700

    configured mongoose to check env variable first

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI ||  "mongodb://localhost/nytreact" || "mongodb://zane:zane123@ds141661.mlab.com:41661/nytimes-search");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
 
 // Start the API server
 app.listen(PORT, function() {
